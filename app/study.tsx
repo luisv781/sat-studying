9d4565d55e5c5e02b577c628d969a73c9c8804c8
luis <89324870+luisv781@users.noreply.github.com>
2025-08-20 21:06:33 +0000

    study screen ( wip :) )

--- app/study.tsx
+++ app/study.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { View } from 'react-native'
+import { Text, useTheme } from 'react-native-paper'
 
 function Study() {
+  const theme = useTheme();
+
   return (
-    <div>Study</div>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Text variant="titleLarge" style={{fontSize: 72, fontWeight: 600}}>Study</Text>
+    </View>
   )
 }
 
