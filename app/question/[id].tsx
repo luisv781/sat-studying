e1675af518a9a8bd5b25f710b630ffc35b1a383e
luis <89324870+luisv781@users.noreply.github.com>
2025-08-22 22:25:18 +0000

    Designing Question Page

--- app/question/[id].tsx
+++ app/question/[id].tsx
@@ -2,8 +2,8 @@ import useFetch from '@/hooks/useFetch';
 import { getQuestion } from '@/services/api';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { ScrollView, View } from 'react-native';
-import { ActivityIndicator, Text, useTheme } from 'react-native-paper';
+import { Platform, ScrollView, View, Text } from 'react-native';
+import { ActivityIndicator, useTheme } from 'react-native-paper';
 import { WebView } from 'react-native-webview';
 
 const Question = () => {
@@ -23,29 +23,56 @@ const Question = () => {
             }}
             className='flex-1 justify-center items-center gap-6'
         >
-            <Text className='text-4xl'>Question:</Text>
+            <Text className='pt-12 text-4xl font-medium text-white'>Random Question</Text>
 
             {fetchError ? (
-                <Text>error</Text>
+                <Text className='text-red-500'>{fetchError.message}</Text>
             ) : questionLoading ? (
-                <ActivityIndicator size={'large'} />
+                <ActivityIndicator size={72} className='p-16' />
             ) : (
-                <ScrollView>
-                    <Text className='text-xl'>
-                        Question Title:
-                    </Text>
-                    <WebView
-                        originWhitelist={['*']}
-                        source={{ html: questionData?.stem ? questionData?.stem : '' }}
-                    />
-                    <Text className='text-xl'>
-                        Correct Answer: {questionData?.correct_answer}
-                    </Text>
-                    <Text className='text-xl'>Rationale:</Text>
-                    <WebView
-                        originWhitelist={['*']}
-                        source={{ html: questionData?.rationale ? questionData?.rationale : '' }}
-                    />
+                <ScrollView className='w-full px-48'>
+                    <View className='flex flex-col py-8 gap-4'>
+                        <Text className='text-2xl text-white'>Question:</Text>
+
+                        {Platform.OS === 'web' ? (
+                            <iframe
+                                className='bg-white rounded-lg h-24'
+                                srcDoc={questionData?.stem}
+                            ></iframe>
+                        ) : (
+                            <WebView
+                                originWhitelist={['*']}
+                                source={{
+                                    html: questionData?.stem
+                                        ? questionData?.stem
+                                        : '',
+                                }}
+                            />
+                        )}
+                    </View>
+
+                    <View className='flex flex-col py-8 gap-4'>
+                        <Text className='text-2xl text-white'>
+                            Correct Answer: {questionData?.correct_answer}
+                        </Text>
+                        <Text className='text-2xl text-white'>Rationale:</Text>
+
+                        {Platform.OS === 'web' ? (
+                            <iframe
+                                className='bg-white rounded-lg h-72'
+                                srcDoc={questionData?.rationale}
+                            ></iframe>
+                        ) : (
+                            <WebView
+                                originWhitelist={['*']}
+                                source={{
+                                    html: questionData?.rationale
+                                        ? questionData?.rationale
+                                        : '',
+                                }}
+                            />
+                        )}
+                    </View>
                 </ScrollView>
             )}
         </View>
