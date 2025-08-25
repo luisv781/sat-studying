ed84fdc9cf28a30eaa426b7b79eb2206938a1500
Luis <luis.villela021@gmail.com>
2025-08-24 19:58:03 -0500

    interfaces

--- interfaces/interfaces.d.ts
+++ interfaces/interfaces.d.ts
@@ -1,6 +1,32 @@
 interface QuestionData {
     stem: string;
+    stimulus?: string; // Only on reading questions
     rationale: string;
     correct_answer: string[];
 }
 
+/**
+ * Question Domains:
+ * 
+ * INI - Information and Ideas
+ * CAS - Craft and Structure
+ * EOI - Expression of Ideas
+ * SEC - Standard English Conventions
+ * H - Algebra
+ * P - Advanced Math
+ * Q - Problem-Solving and Data Analysis
+ * S - Geometry and Trigonometry
+ */
+
+interface QuestionDescription {
+    external_id: string; // Long question ID
+    questionId: string; // Short question ID
+    skill_desc: string; // Skill
+    createDate: number; // Creation date in ms
+    primary_class_cd: string; // Question Domain
+    primary_class_cd_desc: string; // Domain Description
+    difficulty: string; // Difficulty
+    score_band_range_cd: number; // Difficulty level
+}
+
+type QuestionsList = QuestionDescription[];
