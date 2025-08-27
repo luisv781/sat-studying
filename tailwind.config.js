978a0068d2e370d896b372fa75b5d468c8efb3c8
luis <89324870+luisv781@users.noreply.github.com>
2025-08-27 16:56:33 +0000

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import { platformSelect } from "nativewind/theme";
+
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: platformSelect({
+          android: 'Inter_400Regular',
+          ios: 'Inter-Regular'
+        })
+      }
+    },
   },
   plugins: [],
 }
