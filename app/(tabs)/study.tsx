6cb83e6b2461a314cb4fc34db46524da79f9e848
luis <89324870+luisv781@users.noreply.github.com>
2025-08-28 20:48:53 +0000

    small redesign

--- app/(tabs)/study.tsx
+++ app/(tabs)/study.tsx
@@ -1,8 +1,8 @@
 import { getQuestions } from '@/services/api';
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { View } from 'react-native';
-import { Button, Text, useTheme } from 'react-native-paper';
+import { View, Text } from 'react-native';
+import { Button, Card, useTheme } from 'react-native-paper';
 
 const Study = () => {
     const theme = useTheme();
@@ -11,40 +11,54 @@ const Study = () => {
     return (
         <View
             style={{ backgroundColor: theme.colors.background }}
-            className='flex-1 justify-center items-center gap-8'
+            className='flex-1 justify-start items-stretch gap-4'
         >
-            <Text
-                variant='titleLarge'
-                style={{ fontSize: 72, fontWeight: 600, padding: 16 }}
-            >
+            <Text className='mt-8 m-2 text-7xl text-center font-semibold text-white'>
                 Study
             </Text>
-            <Button
-                mode='contained'
-                icon={'book'}
-                onPress={() =>
-                    getQuestions('INI,CAS,EOI,SEC,H,P,Q,S')
-                        .then((questions) => {
-                            if (questions.length > 0) {
-                                const randomQuestion =
-                                    questions[
-                                        Math.floor(
-                                            Math.random() * questions.length
-                                        )
-                                    ];
-                                return randomQuestion.external_id;
-                            } else {
-                                console.warn('No questions available');
-                            }
-                        })
-                        .then((questionId) => {
-                            if (questionId)
-                                router.push(`/question/${questionId}`);
-                        })
-                }
-            >
-                Try Random Question
-            </Button>
+            <Card style={{ margin: 32, marginHorizontal: 64, padding: 24 }}>
+                <Text className='text-4xl font-medium text-white'>
+                    Random Question
+                </Text>
+                <Text className='my-4 text-xl font-normal text-white'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quae, dolor aperiam saepe vitae quod maxime voluptatum
+                    quaerat laborum nemo sapiente earum, quas consequatur
+                    distinctio at minima vel, unde vero soluta? Ducimus rem
+                    doloremque, magni deserunt eveniet veniam dolorum illum
+                    nesciunt molestiae, corrupti voluptas sit architecto, vel ea
+                    incidunt! Sint, enim!
+                </Text>
+                <Card.Actions>
+                    <Button
+                        mode='contained'
+                        icon={'book'}
+                        onPress={() =>
+                            getQuestions('INI,CAS,EOI,SEC,H,P,Q,S')
+                                .then((questions) => {
+                                    if (questions.length > 0) {
+                                        const randomQuestion =
+                                            questions[
+                                                Math.floor(
+                                                    Math.random() *
+                                                        questions.length
+                                                )
+                                            ];
+                                        return randomQuestion.external_id;
+                                    } else {
+                                        console.warn('No questions available');
+                                    }
+                                })
+                                .then((questionId) => {
+                                    if (questionId)
+                                        router.push(`/question/${questionId}`);
+                                })
+                        }
+                    >
+                        Try Random Question
+                    </Button>
+                </Card.Actions>
+            </Card>
         </View>
     );
 };
